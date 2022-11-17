d9bb2f9599a2ad539775dedf86a8f1883df38647
xoctavio <xavieroctavio91@hotmail.com>
2022-11-17 11:43:00 -0600

    Add variables to script.js
    - Created currentCount, totalCount, and count variables
    - Logged currentCount and totalCount to the console

---
+++ script.js
@@ -0,0 +1,7 @@
+// variables
+let currentCount = document.getElementById("count");
+let totalCount = document.getElementById("total");
+let count = 0;
+
+console.log(currentCount);
+console.log(totalCount);
